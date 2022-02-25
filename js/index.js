98ed434c75ee046ece9d5e90bf12849e077f1113
Diego <diegojairhl@gmail.com>
2022-02-25 00:22:47 -0500

    Almacenamiento en la memoria interna agregado, id de button operators en index.html, cambio de clase en el boton cero, reducción de variables, addEventListener independiente para cada operator y limpieza de comentarios innecesarios

--- js/index.js
+++ js/index.js
@@ -21,22 +21,62 @@ const documentReady = () => {
     }
   };
 
-  // const printContent = (content) => {
-  //   content === ""
-  //     ? (valueElement.innerText = content)
-  //     : (valueElement.innerText += formatNumber(content));
-  // };
   // const reverseFormattedNumber = (formattedNumber) => {
   //   return Number(formattedNumber.replace(/,/g, ""));
   // };
+  // Variables
+  let valueStrInMemory = null;
+  let operatorInMemory = null;
 
   // OPERATOR BUTTONS
-  const operators = [...document.querySelectorAll(".calculator__body-button-operator")];
+  const handleOperatorClick = (operator) => {
+    const currentValueStr = getValueAsStr();
+    const currentValueNumber = getValueAsNum();
 
-  operators.map((element) => {
-    element.addEventListener("click", (e) => {
-      console.log(`clicked:  ${e.target.id}`);
-    });
+    if (!valueStrInMemory) {
+      valueStrInMemory = currentValueStr;
+      operatorInMemory = operator;
+      setStrAsValue("0");
+      return;
+    }
+    const valueNumberInMemory = parseFloat(valueStrInMemory);
+    let newValueNumber;
+    // TODO: Can you refactor this code on a Switch or another alternative?
+    if (operatorInMemory === "sum") {
+      newValueNumber = valueNumberInMemory + currentValueNumber;
+    } else if (operatorInMemory === "subtraction") {
+      newValueNumber = valueNumberInMemory - currentValueNumber;
+    } else if (operatorInMemory === "division") {
+      newValueNumber = valueNumberInMemory / currentValueNumber;
+    } else if (operatorInMemory === "multiplication") {
+      newValueNumber = valueNumberInMemory * currentValueNumber;
+    }
+    valueStrInMemory = newValueNumber.toString();
+    operatorInMemory = operator;
+    setStrAsValue("0");
+    console.log(valueStrInMemory);
+    console.log(operatorInMemory);
+  };
+
+  const sum = document.getElementById("sum");
+  const subtraction = document.getElementById("subtraction");
+  const multiplication = document.getElementById("multiplication");
+  const division = document.getElementById("division");
+
+  sum.addEventListener("click", () => handleOperatorClick("sum"));
+
+  subtraction.addEventListener("click", () => handleOperatorClick("subtraction"));
+
+  multiplication.addEventListener("click", () => handleOperatorClick("multiplication"));
+
+  division.addEventListener("click", () => handleOperatorClick("division"));
+
+  const equal = document.getElementById("equal");
+  equal.addEventListener("click", () => {
+    if (!valueStrInMemory) {
+      valueStrInMemory = null;
+      operatorInMemory = null;
+    }
   });
 
   // NUMBERS
@@ -69,7 +109,9 @@ const documentReady = () => {
 
   // CLEAR
   const handleClearClick = () => {
-    valueElement.textContent = "0";
+    setStrAsValue("0");
+    valueStrInMemory = null;
+    operatorInMemory = null;
   };
 
   const clear = document.getElementById("clear");
@@ -80,14 +122,18 @@ const documentReady = () => {
 
   back.addEventListener("click", () => {
     const valueStr = getValueAsStr();
+
     const valueArr = valueStr.split("");
+
     valueArr.shift();
+
     const valueArrBacked = valueArr.join("");
     if (valueArr.length === 0) {
-      return handleClearClick();
+      return setStrAsValue("0");
     }
     valueElement.textContent = valueArrBacked;
   });
+  console.log(valueStrInMemory);
 };
 
 document.addEventListener("DOMContentLoaded", documentReady);
